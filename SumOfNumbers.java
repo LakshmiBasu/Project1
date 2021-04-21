76ff62dc6d883278aec842d22f8ba525c2ca7464
lakshmigopakumar0404 <62078348+lakshmigopakumar0404@users.noreply.github.com>
2021-04-21 15:29:23 -0400

    Update SumOfNumbers.java
    I have added a new print statement at the end

--- SumOfNumbers.java
+++ SumOfNumbers.java
@@ -4,7 +4,8 @@ public class SumOfNumbers {
 		int a = 2;
 		int b = 3;
 		
-		System.out.println(a+b);		
+		System.out.println(a+b);
+		System.out.println("This is a modification from a forked repository by a different user");
 	}
 
 }
